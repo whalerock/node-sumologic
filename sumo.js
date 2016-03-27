78c062feed41ccd84986f2229146b248467272a2
Jeeva Suresh <jeeva@helixta.com.au>
2016-03-27 14:38:45 +1100

    Implementation of sync, with tests

---
+++ sumo.js
@@ -0,0 +1,97 @@
+// Log to sumo logic directly
+
+var _ = require('underscore');
+
+function safeToString(obj) {
+  try {
+    return JSON.stringify(obj);
+  } catch (err) {
+    try {
+      return JSON.stringify(String(obj));
+    } catch (err) {
+      return JSON.stringify('error serializing log line');
+    }
+  }
+}
+
+module.exports = function SumoLogger(collectorCode, opts) {
+  var me = this;
+
+  opts = opts || {};
+  var endpoint = opts.endpoint || "https://collectors.au.sumologic.com/receiver/v1/http/";
+  var request = opts.request || require('request');
+  var collectorEndpoint = endpoint + collectorCode;
+  var syncInterval = opts.syncInterval || 1000;
+
+  me.replaceConsole = function() {
+    console.log = me.log;
+    console.info = me.info;
+    console.error = me.error;
+    console.warn = me.warn;
+  }
+
+  // Cache of entries we are yet to sync
+  var unsynced = [];
+
+  function append(lvl, args) {
+    var stringifyArgs = _.map(args, function(a) {
+      return safeToString(a);
+    });
+
+    // In the common case of a single log value, pull it out. It's easier in sumo
+    // logic to traverse known object graphs without arrays, especially at the
+    // top level
+    var data = ''
+    if (stringifyArgs.length == 1) {
+      data = stringifyArgs[0];
+    } else {
+      data = '[' + stringifyArgs.join(', ') + ']';
+    }
+
+    unsynced.push('{"level":' + JSON.stringify(lvl) + ',"data":' + data + '}');
+  }
+
+  // I want arguments to be treated as an object (helps indexing into the correct fields on sumo logic)
+  me.log = function() { append('INFO', arguments); };
+  me.info = function() { append('INFO', arguments); };
+  me.error = function() { append('ERROR', arguments); };
+  me.warn = function() { append('WARN', arguments); };
+
+  var numBeingSent = 0;
+  var maxLines = 100;
+
+  // explicit decision to *not* use debounce/throttle so the syncing code is
+  // explicit, and it's possible for a human to prove it's correctness
+  var syncer = setInterval(function() {
+    // Only one active sync at any given interval
+    if (numBeingSent > 0) {
+      return;
+    }
+
+    // Short-circuit if there is nothing to send
+    if (unsynced.length == 0) {
+      return;
+    }
+
+    var logLines = unsynced.slice(0, maxLines);
+    var body = logLines.join('\n')
+    numBeingSent = logLines.length;
+
+    // Sync logs to sumo-logic, and clear all synced data. On failure we'll retry the sync
+    request({
+      method: 'POST',
+      url: collectorEndpoint,
+      body: body,
+    }, function(error, response) {
+      var failed = !!error || 
+        response.status < 200 || 
+        response.status >= 400;
+
+      if (!failed) {
+        unsynced.splice(0, numBeingSent);
+      }
+
+      numBeingSent = 0;
+    })
+  }, syncInterval);
+}
---
+++ test/sumo-test.js
@@ -0,0 +1,198 @@
+var assert = require('assert');
+var sinon = require('sinon');
+var chai = require('chai')
+  , expect = chai.expect;
+var SumoLogger = require('../sumo.js');
+
+function newTestSumoLogger(fakeRequest) {
+  return new SumoLogger('FAKE-COLLECTOR-CODE', fakeRequest);
+}
+
+describe('Sumo Logic Collector', function() {
+  var clock;
+  beforeEach(function() { clock = sinon.useFakeTimers(); })
+  afterEach(function() { clock.restore(); })
+
+  function requestShouldntBeCalled() {
+    throw new Error("Unexpected call to request");
+  }
+
+  it("No sync for first second", function () {
+    var sumologic = newTestSumoLogger(requestShouldntBeCalled);
+    sumologic.log("log line");
+
+    clock.tick(1);
+    clock.tick(998);
+  });
+
+  it("No sync if there's no log data", function () {
+    var sumologic = newTestSumoLogger(requestShouldntBeCalled);
+
+    clock.tick(1);
+    clock.tick(1000);
+    clock.tick(1000);
+  });
+
+  it("Only one in progress request at any time", function () {
+    var count = 0
+    var sumologic = newTestSumoLogger(function() {
+      count += 1;
+      assert.equal(1, count);
+    });
+
+    sumologic.log("log line");
+
+    // First call to request
+    clock.tick(1000);
+
+    // Shouldn't call request, as we have an active call
+    clock.tick(1000);
+  });
+
+  it("Failed requests should be retried first", function () {
+    var expectedBody = ''
+    var sumologic = newTestSumoLogger(function(opts, cb) {
+      assert.equal(expectedBody , opts.body);
+      cb("some error");
+    });
+
+    sumologic.log("log line");
+
+    // First call to request, we should get a single line
+    expectedBody = '{"level":"INFO","data":"log line"}',
+    clock.tick(1000);
+
+    // Retry attempt - should have the same body as the previous request
+    expectedBody = '{"level":"INFO","data":"log line"}',
+    clock.tick(1000);
+
+    // Third retry attempt, should have 2 lines to sync (the original and the new line just added)
+    sumologic.log("new log line");
+    expectedBody = '{"level":"INFO","data":"log line"}\n{"level":"INFO","data":"new log line"}'
+    clock.tick(1000);
+  });
+
+  it("Non 200/300 status codes are treated as errors", function () {
+    var expectedBody = '';
+    var nextStatus = 0;
+    var sumologic = newTestSumoLogger(function(opts, cb) {
+      assert.equal(expectedBody, opts.body);
+      cb(undefined, {status: nextStatus});
+    });
+
+    sumologic.log("log line");
+
+    // First call to request, we should get a single line
+    nextStatus = 100; 
+    expectedBody = '{"level":"INFO","data":"log line"}'; 
+    clock.tick(1000);
+
+    // Retry attempt - should have the same body as the previous request
+    nextStatus = 400; 
+    expectedBody = '{"level":"INFO","data":"log line"}'; 
+    clock.tick(1000);
+
+    // Third retry attempt, should have 2 lines to sync (the original and the new line just added)
+    sumologic.log("new log line");
+    nextStatus = 500; 
+    expectedBody = '{"level":"INFO","data":"log line"}\n{"level":"INFO","data":"new log line"}'
+    clock.tick(1000);
+
+    // attempts from now on sync all data, then request should no longer be called (as there are no log lines)
+    nextStatus = 200; 
+    expectedBody = '{"level":"INFO","data":"log line"}\n{"level":"INFO","data":"new log line"}'
+    clock.tick(1000);
+
+    nextStatus = 200; 
+    expectedBody = "ERROR, request shouldn't be called anymore";
+    clock.tick(1000);
+  });
+
+  it("Expected happy case of a few logs being synced every tick", function () {
+    var expectedLogLines = [];
+    var sumologic = newTestSumoLogger(function(opts, cb) {
+      expect(opts.body.split('\n')).to.deep.equal(expectedLogLines);
+      cb(undefined, {status: 200});
+    });
+
+    function logLine(line) {
+      expectedLogLines.push(JSON.stringify({level: 'INFO', data: line}));
+      sumologic.log(line);
+    }
+
+    function runSync() {
+      clock.tick(1000);
+      expectedLogLines = [];
+    }
+
+    logLine("msg 1");
+    logLine("msg 2");
+    logLine("msg 3");
+    runSync();
+
+    logLine("msg 3");
+    runSync();
+
+    logLine("msg 4");
+    logLine("msg 5");
+    logLine("msg 6");
+    logLine("msg 7");
+    runSync();
+  });
+
+  it("Exercise safeToString", function () {
+    var expectedBody = ''
+    var sumologic = new SumoLogger(function(opts, cb) {
+      expect(opts.body).to.equal(expectedBody);
+      cb(undefined, {status: 200});
+    });
+
+    function checkObject(line, expected) {
+      expectedBody = expected || JSON.stringify({level: 'INFO', data: line});
+      sumologic.log(line);
+      clock.tick(1000);
+    }
+
+    checkObject("msg 1");
+    checkObject({});
+
+    // circular refs will confuse JSON.stringify, fallback to toString
+    x = {};
+    x.y = x;
+    checkObject(x, '{"level":"INFO","data":"[object Object]"}');
+
+    // Finally, check errors in toString are handled
+    function TestObj() { }
+    TestObj.prototype.toJSON = function() { throw new Error(); }
+    TestObj.prototype.toString = function() { throw new Error(); }
+    checkObject(new TestObj(), '{"level":"INFO","data":"error serializing log line"}');
+  });
+
+  it("Ensure log lines are valid json", function () {
+    var expected = {};
+    var sumologic = newTestSumoLogger(function(opts, cb) {
+      expect(JSON.parse(opts.body)).to.deep.equal({level: "INFO", data: expected});
+      cb(undefined, {status: 200});
+    });
+
+    function check(obj) {
+      expected = obj;
+      sumologic.log(obj);
+      clock.tick(1000);
+    }
+
+    check("msg 1");
+    check({some: "values", and: "keys"});
+    check([1,2,3,4]);
+
+    function checkVarargs(obj1, obj2) {
+      expected = [obj1, obj2];
+      sumologic.log(obj1, obj2);
+      clock.tick(1000);
+    }
+
+    checkVarargs("msg 1", "msg 2");
+    checkVarargs("some string", {some: "values", and: "keys"});
+    checkVarargs("some string", [1,2,3,4], {});
+  });
+});
